904d26d64e0b8da6daf0aa5b16f5c794918fa298
unknown <anhnt.tb12@gmail.com>
2022-02-20 22:21:48 +0700

    display emlpoyee list

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import Model.Employee;
+
+public class Main {
+    public static Employee[] employees;
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Model/FulltimeEmployee.java
@@ -0,0 +1,61 @@
+package Model;
+
+public class FulltimeEmployee extends Employee {
+    private int bonus;
+    private int finemoney;
+    private int baseSalary;
+
+    @Override
+    public double getNetSalary() {
+        return 0;
+    }
+
+    public FulltimeEmployee() {
+    }
+
+    public FulltimeEmployee(int bonus, int finemoney, int baseSalary) {
+        this.bonus = bonus;
+        this.finemoney = finemoney;
+        this.baseSalary = baseSalary;
+    }
+
+    public FulltimeEmployee(String employeeId, String fullName, int age, String phoneNumber, String email, int bonus, int finemoney, int baseSalary) {
+        super(employeeId, fullName, age, phoneNumber, email);
+        this.bonus = bonus;
+        this.finemoney = finemoney;
+        this.baseSalary = baseSalary;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(int bonus) {
+        this.bonus = bonus;
+    }
+
+    public int getFinemoney() {
+        return finemoney;
+    }
+
+    public void setFinemoney(int finemoney) {
+        this.finemoney = finemoney;
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(int baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    @Override
+    public String toString() {
+        return "FulltimeEmployee{" +
+                "bonus=" + bonus +
+                ", finemoney=" + finemoney +
+                ", baseSalary=" + baseSalary +
+                '}';
+    }
+}
